76bc19ae1fdca4e0499d77dd8439b08c2684bb9a
Hritiksonkar <147808870+Hritiksonkar@users.noreply.github.com>
2025-03-21 23:53:03 +0530

    vaiable

---
+++ Javascript/variable.js
@@ -0,0 +1,14 @@
+const accuntId =14452
+let accuntEmail="hritikbhai73@gmail.com"
+var accuntPassword=12345
+accuntcity="delhi"
+let accuntstate 
+
+// accuntId=2 not allowed
+accuntEmail="hri7@gmial.com"
+accuntPassword="12121"
+accuntcity="hariyana"
+console.log(accuntId);
+// prefer not to use var 
+// because of issue in block scope and function scope
+console.table([accuntEmail,accuntPassword,accuntcity,accuntstate])
