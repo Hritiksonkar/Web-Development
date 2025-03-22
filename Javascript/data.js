03631acb40093f83646e700e8dca70cdddee6338
Hritiksonkar <147808870+Hritiksonkar@users.noreply.github.com>
2025-03-22 11:46:50 +0530

    datatype

---
+++ Javascript/data.js
@@ -0,0 +1,17 @@
+ "use strict"; //treat all js code as newer version 
+//  alert(3+3) //we are useing nodejs,not browser 
+ console.log(3+3);
+ console.log("hritik");
+
+ let name="hritik"
+ let age=20
+ let isLongint=false
+// //  Number => 2to power 
+// //  BigInt
+//  string=>""
+//   Boolean=>true/false 
+//   null=> standalonne value 
+// undefined
+// Symbol => unique
+// object 
+console.log(typeof  age);
