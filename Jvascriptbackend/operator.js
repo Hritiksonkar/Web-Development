0f1a69e0c5fd3e527c573761198d714cbd4f70c9
Hritiksonkar <147808870+Hritiksonkar@users.noreply.github.com>
2025-03-22 20:18:24 +0530

    variable and operator

---
+++ Jvascriptbackend/operator.js
@@ -0,0 +1,47 @@
+// mathamatical operators
+let x=5;
+let y=3;
+console.log(x+y);
+console.log(x-y);
+console.log(x%y);
+console.log(x/y);
+console.log(x**y);
+// assinment operator 
+let a=10
+a=a+5
+console.log(a);
+// comprision operator
+let p=10;
+let q=15;
+console.log(p==q)
+console.log(p===q)
+console.log(p!=q)
+console.log(p<q)
+console.log(p>q)
+console.log(p<=q)
+// logical operator
+let e =true;
+let f=false;
+console.log(e&&f);
+console.log(e||f);
+console.log(!e);
+console.log(!f);
+// Ternary operator 
+let age =18
+let isAdult=(age>=18) ? "yes ,adult !":"no,stillakid";
+console.log(isAdult)
+// Bitwise operator 
+let m=5
+let n =3 
+console.log(m&n);
+console.log(m|n);
+console.log(m^n);
+console.log(5<<1)
+console.log(5>>1)
+let s=50;
+s=s++;
+console.log(s)
+
+
+
+
---
+++ Jvascriptbackend/variable.js
@@ -0,0 +1,28 @@
+ let  name = "vishva"
+  const child = "hritik"
+ name ="hritik"
+console.log(typeof child);
+// naming conversion  of  a variable  (a_z A_Z _,)
+// Boolean
+let anAProfassoinal=true;
+let amILate=false 
+//undefine
+let school ;
+console.log(school);
+//null 
+let hobby =null;
+console.log(typeof null);
+//symbole 
+let  id=Symbol("id")
+let  id2=Symbol("id")
+console.log(id===id2);
+//arry 
+let color=['red','black','blue'];
+let num =[4,5,5,3,5,3];
+
+//object
+let person={
+name :"hrtik",
+age :38
+}
+console.log(typeof num);
