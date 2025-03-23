a5452cb70637314edcd819518146a888191e7062
Hritiksonkar <147808870+Hritiksonkar@users.noreply.github.com>
2025-03-23 23:22:34 +0530

    function

---
+++ Jvascriptbackend/function.js
@@ -0,0 +1,48 @@
+//  function helloboy() {
+//     console.log("hello function1"); 
+//  } 
+//  helloboy()
+// //  function that parametor
+// function add(a,b) {
+//     return a+b; 
+// }
+// let sum=add(5,6)
+// console.log(sum);
+ 
+// const multiply=function(a,b){
+// return(a*b)
+// }
+// const multi_new=multiply
+// console.log(multiply(5,7));
+
+// function greed(name="hritika") {
+//     console.log("hello"+name);
+// }
+// greed("vishva sir")
+// greed();
+// // rest parameter
+// function  small(...nums) { 
+//     console.log(nums)
+//     console.log(...nums);
+    
+//     let sum =0
+//     for (let  i = 0;  i <nums;  i++) {
+//          sum +=nums[i]
+        
+//     } 
+//     return sum 
+// }
+// console.log(small(1,2,3,4,5,));
+
+// function test() {
+//     console.log("insider test");
+    
+// }
+// test(1)
+// no parameter
+const greed1=()=> console.log ("hello student")
+const add = (num1,num2) => num1+ num2;
+console.log(add (5,7))
+mul
+
+
