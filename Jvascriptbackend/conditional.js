b40f36f1a210ed2c05d33b59b7ecdae0cde8f48a
Hritiksonkar <147808870+Hritiksonkar@users.noreply.github.com>
2025-03-23 19:34:28 +0530

    condition statement

---
+++ Jvascriptbackend/conditional.js
@@ -0,0 +1,79 @@
+let age=18
+if (age<=18) {
+    console.log("i am adult");
+}
+else if(age>=20&&age<50){
+    console.log( "i am a teen");
+}else{
+
+    console.log("i am a kid")
+}
+let age2 =21 ;
+let hastiket =true
+if (age2<=21) {
+    if (hastiket) {
+        console.log("allow the party");
+        
+    }
+    else{
+        console.log("not allowed the paty");
+        
+    }
+}
+// swich statement
+let day=1
+switch (day) {
+    case 1:
+        console.log( "monday");
+        
+        break;
+    case 2:
+        console.log("tuesday");
+        
+        break;
+     case 3:
+            console.log("wednesday");
+            
+            break;
+         case 4:
+                console.log ("(thirsday");
+                
+                break;
+      case 5:
+                    console.log("friday");
+                    
+                    break;
+     case 6:
+                        console.log(saiterday);
+                        
+                        break;
+     case 7:
+                         console.log(sunday);
+                            
+                            break;
+                                                                 
+
+    default:
+        console.log("oppp day are not present");
+        
+        break;
+}
+// // loop statement
+for (let i =0; i<=5; i++) {
+    // const element = array[i];
+    console.log(i);  
+}
+// while loop 
+let num=0
+while (num>0) {
+    console.log("hello from hritika");   
+}
+// do while loop
+let i=3;
+do{
+console.log(i);
+i--;
+
+}while(i>0);
+
+
