9821495e336533d4508c0bf17d32e3a6dcfe8a7e
Hritiksonkar <hritiksonkar355@gmail.com>
2025-06-12 17:00:31 +0530

    Add new text files and update script.js to include file reading functionality

--- Jvascriptbackend/nodejs/script.js
+++ Jvascriptbackend/nodejs/script.js
@@ -1,2 +1,54 @@
-const hello = "Hello, World!";
-console.log(hello);
+// const hello = "Hello, World!";
+// console.log(hello);
+
+// const { log } = require('node:console');
+const fs = require('node:fs');
+// fs.writeFile("hay.txt","hey hello kaise ho ",function(err){
+//     if(err)console.error(err);
+//     else log("File written successfully");
+// })
+
+// fs.appendFile("hay.txt", "hello world", function(err) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log("File updated successfully");
+//     }
+// });
+
+// fs.rename("hay.txt", "hay1.txt", function(err) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log("File renamed successfully");
+//     }
+// });
+
+// fs.copyFile("hay1.txt", "./copy/devloper.txt", function(err) {
+//     if (err) console.error(err);
+//     else console.log("File copied successfully");
+    
+// })
+// fs.unlink("hay1.txt", function(err) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log("File deleted successfully");
+//     }
+// });
+
+// fs.rmdir*("copy", { recursive: true }, function(err) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log("Directory deleted successfully");
+//     }
+// } );
+
+fs.readFile("hay1.txt", "utf-8", function(err, data) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("File content:", data);
+    }
+});
