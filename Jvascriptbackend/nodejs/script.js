a5d1fffab6cfb8e325a230869253542ff765a3e7
Hritiksonkar <hritiksonkar355@gmail.com>
2025-07-02 18:25:29 +0530

    initial program

--- Jvascriptbackend/nodejs/script.js
+++ Jvascriptbackend/nodejs/script.js
@@ -53,11 +53,51 @@
 //     }
 // });
 
-const http = require("http");
-const server=http.createServer(function(req, res) {
-    res.end("Hello, World!");
-}
-);
-server.listen(3000, function() {
-    console.log("Server is running on port 3000");
+// const http = require("http");
+// const server=http.createServer(function(req, res) {
+//     res.end("Hello, World!");
+// }
+// );
+// server.listen(3000, function() {
+//     console.log("Server is running on port 3000");
+// });
+//    start express
+
+// const express = require("express");
+//  const app=express()
+//  app.get("/", function(req, res) {
+//      res.send("Hello, World kahse !");
+//  })
+//  app.get("/profile", function(req, res) {
+//      res.send("Hello, World1!");
+//  })
+//  app.listen(3000);
+
+// middleware
+const express = require("express");
+const app = express();
+app.use(function(req, res, next) {
+    console.log("Middleware executed");
+    next(); // Call the next middleware or route handler
 });
+app.use(function(req, res, next) {
+    console.log("Middleware executed2 time");
+    next();
+});
+app.get("/", function(req, res) {
+    res.send("Hello, World!");
+});
+app.get("/about", function(req, res) {
+    res.send("about page hai ye!");
+});
+//reqvest and response haindling
+app.get("/profile", function(req, res, next) {
+    return next(new Error("Profile not found"));
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+app.listen(3000);
+ 
+ 
